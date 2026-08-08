7ce439579e38c79dc81eb6b1300e09b1694d25f7
mihav <mvelickovic03@proton.me>
2026-08-08 17:45:37 +0200

    WIP: admin working on editing the user temp on hold until everything about the model is figured out

--- client/src/components/UserCard.tsx
+++ client/src/components/UserCard.tsx
@@ -3,19 +3,25 @@ import User from "../models/User.ts";
 export const UserCard = ({user}: {user: User}) => {
     return (
         <div className="userCard">
-            <div className="info">
-                <p>Username: </p>
-                <p>{user.username}</p>
+            <div className="userInfo">
+                <div className="info">
+                    <p>Username: </p>
+                    <p>{user.username}</p>
+                </div>
+                <div className="info">
+                    <p>Email: </p>
+                    <p>{user.email}</p>
+                </div>
+                <div className="info">
+                    <p>Admin Status: </p>
+                    <p>{user.admin ? "True" : "False"}</p>
+                </div>
             </div>
-            <div className="info">
-                <p>Email: </p>
-                <p>{user.email}</p>
+            <div className="buttons">
+                <button className="button">Toggle Admin</button>
+                <button className="button">Reset Password</button>
+                <button className="button delete">Delete</button>
             </div>
-            <div className="info">
-                <p>Admin Status: </p>
-                <p>{user.admin ? "True" : "False"}</p>
-            </div>
-            
         </div>
     )
 }
